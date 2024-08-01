85c2d2984ece83afd3cc4ed51ab09996a77bb86e
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-08-01 14:04:28 +0500

    home changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,10 +23,10 @@ const Home = () => {
     <div className='flex items-center justify-center w-full h-screen'>
       <div className='grid gap-10'>
         <h1 className='text-5xl text-blue-600 text-center font-bold'>Kursni tanlang</h1>
-        <div className='grid grid-cols-3 gap-5'>
+        <div className='grid grid-cols-3 gap-5 max-sm:grid-cols-2'>
           {state.map((product) => {
             return (
-              <div onClick={() => navigate(`/${product.href}`)} key={product.id} className='cursor-pointer hover:bg-blue-700 hover:shadow-md hover:shadow-blue-600 transition-all px-5 py-3 rounded-xl bg-blue-600'>
+              <div onClick={() => navigate(`/${product.href}`)} key={product.id} className='cursor-pointer hover:bg-blue-700 hover:shadow-md hover:shadow-blue-600 transition-all px-5 py-3 max-sm:flex max-sm:justify-center rounded-xl bg-blue-600'>
                 <h1 className='text-lg font-medium text-white'>{product.name}</h1>
               </div>
             )
